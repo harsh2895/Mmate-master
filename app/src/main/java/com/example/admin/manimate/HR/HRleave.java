11c1035d4482352cd30ba14db6decc5410fc5717
harsh2895 <hvaghela138@hotmail.com>
2017-04-11 16:17:49 +0530

    second time commite

--- app/src/main/java/com/example/admin/manimate/HR/HRleave.java
+++ app/src/main/java/com/example/admin/manimate/HR/HRleave.java
@@ -2,8 +2,15 @@ package com.example.admin.manimate.HR;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.admin.manimate.R;
+
+import static com.example.admin.manimate.R.id.HRJobtital;
+import static com.example.admin.manimate.R.id.HRleaveSub;
 
 public class HRleave extends AppCompatActivity {
 
@@ -11,5 +18,31 @@ public class HRleave extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hrleave);
+        final EditText ed1=(EditText)findViewById(HRleaveSub);
+        final EditText ed2=(EditText)findViewById(R.id.HRleaveDate);
+        final EditText ed3=(EditText)findViewById(R.id.HrleaveDec);
+        Button button=(Button)findViewById(R.id.BtHRleaveSub) ;
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String HRleavesub = ed1.getText().toString();
+                String HRleavedate = ed2.getText().toString();
+                String HRleavedec = ed3.getText().toString();
+
+                if (HRleavesub.isEmpty()) {
+                    Toast.makeText(HRleave.this, "UserName can't be empty", Toast.LENGTH_SHORT).show();
+                } else if (HRleavedate.isEmpty()) {
+                    Toast.makeText(HRleave.this, "Emailid can't be empty", Toast.LENGTH_SHORT).show();
+
+                } else if (HRleavedec.isEmpty()) {
+                    Toast.makeText(HRleave.this, "mobile can't be empty", Toast.LENGTH_SHORT).show();
+
+
+                }
+            }
+
+
+        });
     }
 }
---
+++ app/src/main/java/com/example/admin/manimate/PM/PMLeave.java
@@ -0,0 +1,47 @@
+package com.example.admin.manimate.PM;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.admin.manimate.R;
+
+import static com.example.admin.manimate.R.id.PmLeavesubj;
+
+public class PMLeave extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pmleave);
+        final EditText ed1=(EditText)findViewById(PmLeavesubj);
+        final EditText ed2=(EditText)findViewById(R.id.PmLeavedat);
+        final EditText ed3=(EditText)findViewById(R.id.PmLeaveDec);
+        Button button=(Button)findViewById(R.id.BtPmsubmit) ;
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String PMLeavesub = ed1.getText().toString();
+                String PMLeavedate = ed2.getText().toString();
+                String PMLeavedec = ed3.getText().toString();
+
+                if (PMLeavesub.isEmpty()) {
+                    Toast.makeText(PMLeave.this, "UserName can't be empty", Toast.LENGTH_SHORT).show();
+                } else if (PMLeavedate.isEmpty()) {
+                    Toast.makeText(PMLeave.this, "Emailid can't be empty", Toast.LENGTH_SHORT).show();
+
+                } else if (PMLeavedec.isEmpty()) {
+                    Toast.makeText(PMLeave.this, "mobile can't be empty", Toast.LENGTH_SHORT).show();
+
+
+                }
+            }
+
+
+        });
+    }
+}
